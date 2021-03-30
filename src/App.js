5827c231687bbc0b08bd7565424bb565cd371c43
Koenomatachi-San <50592755+KoenomatachiSan@users.noreply.github.com>
2021-03-30 19:54:31 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Component from './Component';
+
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+         Hello World!
+         My name is KoenomatachiSan, and it's my component!
+        </p>
+        <Component />
+    </div>
+  );
+}
+
+export default App;
